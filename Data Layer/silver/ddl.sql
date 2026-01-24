f666ac7574122640cfb84857f2e2100e32faecfa
Julia <juliagabpaulino@outlook.com>
2026-01-24 13:41:09 -0300

    fix: corrigindo ddl silver

--- Data Layer/silver/ddl.sql
+++ Data Layer/silver/ddl.sql
@@ -1,56 +1,55 @@
-CREATE TYPE via_urg_enum AS ENUM (
-        'SIM',
-        'NÃO'
-);
-CREATE TYPE mes_enum AS ENUM (
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-);
-CREATE TABLE viagem (
-        ide_pro_via TEXT PRIMARY KEY,
-        
-        nmr_ppt_pcd TEXT,
-        
-        situacao TEXT,
-        
-        via_urg via_urg_enum,
-        
-        jus_urg_via TEXT,
-        
-        cod_org_sup INTEGER,
-        nom_org_sup TEXT,
-        
-        cod_org_sol INTEGER,
-        nom_org_sol TEXT,
-        
-        cpf_vjt TEXT,
-        nome TEXT,
-        cargo TEXT,
-        funcao TEXT,
-        
-        dat_ini DATE,
-        dat_fim DATE,
-        
-        destinos TEXT,
-        
-        motivo TEXT,
-        
-        val_dia NUMERIC(14,2),
-        val_pas NUMERIC(14,2),
-        val_dev NUMERIC(14,2),
-        val_out_gas NUMERIC(14,2),
-	tot_gas NUMERIC(14,2),
-	mes_ida mes_enum,
-	mes_vol mes_enum
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_type WHERE typname = 'via_urg_enum'
+    ) THEN
+        CREATE TYPE via_urg_enum AS ENUM ('SIM', 'NÃO');
+    END IF;
+END$$;
+
+
+-- ===============================
+-- TABELA: viagem (Silver Layer)
+-- ===============================
+CREATE TABLE IF NOT EXISTS viagem (
+    
+    ide_pro_via TEXT PRIMARY KEY,
+    
+    nmr_ppt_pcd TEXT,
+    
+    situacao TEXT,
+    
+    via_urg via_urg_enum,
+    
+    jus_urg_via TEXT,
+    
+    cod_org_sup INTEGER,
+    nom_org_sup TEXT,
+    
+    cod_org_sol INTEGER,
+    nom_org_sol TEXT,
+    
+    cpf_vjt TEXT,
+    nome_vjt TEXT,
+    
+    cargo TEXT,
+    funcao TEXT,
+    
+    dat_ini DATE,
+    dat_fim DATE,
+    
+    destinos TEXT,
+    
+    motivo TEXT,
+    
+    val_dia NUMERIC(18,2),
+    val_pas NUMERIC(18,2),
+    val_dev NUMERIC(18,2),
+    val_out_gas NUMERIC(18,2),
+    
+    tot_gas NUMERIC(18,2),
+    
+    mes_ida SMALLINT CHECK (mes_ida BETWEEN 1 AND 12),
+    mes_vol SMALLINT CHECK (mes_vol BETWEEN 1 AND 12)
 );
 
